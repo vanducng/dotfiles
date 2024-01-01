18faf17460c572de8970ecabd4b40307e28329f2
Duc Nguyen <duc@careernowbrands.com>
2024-01-02 03:02:18 +0700

    chore: Add scripts

---
+++ dotfiles/scripts/macos-deps.sh
@@ -0,0 +1,29 @@
+brew install obsidian
+brew install fzf
+brew install zsh-syntax-highlighting
+brew install firefox
+brew install skhd
+brew install istat-menus
+brew install docker
+brew install docker-compose
+brew install alfred
+brew install trash
+brew install the_silver_searcher
+brew install tree
+brew install yt-dlp
+brew install jsonpp
+brew install jq
+
+brew install exa
+brew install z
+
+# spark, fantastical, cardhop, shush, kaleidoscope
+
+brew install ksdiff
+brew install whatsapp
+# brew install alt-tab
+brew install neovim
+
+brew install fnm
+brew install tmux
+brew install rust
