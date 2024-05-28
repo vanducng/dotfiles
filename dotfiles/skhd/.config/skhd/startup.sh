4ffcc8fba35b0cbb17a757de4c98fa18a159dae9
Duc Nguyen <duc@careernowbrands.com>
2024-05-28 08:20:16 +0700

    chore: Add starup script

---
+++ dotfiles/skhd/.config/skhd/startup.sh
@@ -0,0 +1,7 @@
+open "/Applications/Slack.app" && yabai -m window --space 6
+open "/System/Volumes/Data/Applications/Zalo.app" && yabai -m window --space 5
+open "/Applications/DBeaver.app" && yabai -m window --space 4
+open "/Applications/Visual Studio Code.app" && yabai -m window --space 3
+open "/Applications/Arc.app" && yabai -m window --space 2
+open "/Applications/kitty.app" && yabai -m window --space 1
+open "/Applications/Firefox Developer Edition.app" && yabai -m window --space 1
