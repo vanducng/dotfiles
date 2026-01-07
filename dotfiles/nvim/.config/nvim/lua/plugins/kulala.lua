d722cafa26cde308f25db3e4a8badb88c1763026
Duc Nguyen <duc@careernowbrands.com>
2026-01-07 20:20:36 +0700

    feat(nvim): add kulala.nvim for HTTP request testing

---
+++ dotfiles/nvim/.config/nvim/lua/plugins/kulala.lua
@@ -0,0 +1,14 @@
+---@type LazySpec
+return {
+  "mistweaverco/kulala.nvim",
+  ft = { "http", "rest" },
+  keys = {
+    { "<leader>Rs", desc = "Send request" },
+    { "<leader>Ra", desc = "Send all requests" },
+    { "<leader>Rb", desc = "Open scratchpad" },
+  },
+  opts = {
+    global_keymaps = true,
+    global_keymaps_prefix = "<leader>R",
+  },
+}
